d55e2388f8509e4009038da7892dbb5678326c46
Abena <priscillaabenaaboagye@gmail.com>
2025-07-09 19:56:05 +0000

    Change from int to float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-user_monthly_income = int(input("Enter your monthly income: "))
-user_monthly_expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = user_monthly_income - user_monthly_expenses
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print(f"Your monthly savings are ${int(monthly_savings)}.")
 print(f"Projected savings after one year, with interest, is: ${round(projected_savings)}.")
