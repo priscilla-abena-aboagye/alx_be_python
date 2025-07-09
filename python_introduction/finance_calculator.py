008d4f5bfa16a49dff0c80a5969f12bbe04e15e9
Abena <priscillaabenaaboagye@gmail.com>
2025-07-09 19:52:15 +0000

    Add punctuations to the print function

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,5 +2,5 @@ user_monthly_income = int(input("Enter your monthly income: "))
 user_monthly_expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = user_monthly_income - user_monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print(f"Your monthly savings are ${int(monthly_savings)}")
-print(f"Projected savings after one year, with interest, is ${int(projected_savings)}")
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${round(projected_savings)}.")
