7ceb7f9375f72b8546d56faa808cf3b241879389
Abena <priscillaabenaaboagye@gmail.com>
2025-07-20 16:52:07 +0000

    punctuation

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,7 +4,7 @@ if weather == "sunny":
 elif weather == "rainy":
 	print("Don't forget your umbrella and a raincoat")
 elif weather == "cold":
-	print("Make sure to wear a warm coat and a scarf")
+	print("Make sure to wear a warm coat and a scarf.")
 else:
-	print("Sorry, I don't have recommendation for this weather")
+	print("Sorry, I don't have recommendation for this weather.")
 
