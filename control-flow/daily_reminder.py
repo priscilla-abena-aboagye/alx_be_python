198f4af6d6c78cd9363a31a803cb77928c86bc1a
Abena <priscillaabenaaboagye@gmail.com>
2025-07-20 18:09:11 +0000

    daily reminder using match cases and conditional statement

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+task = input("Enter your task: ")
+priority = input("Priority (high, medium, low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+match priority:
+	case "high":
+		if time_bound == "yes":
+			print(f"{task} is a high priority task that requires immediate attention today!")
+		else:
+			print(f"{task} is a high priority do it when free")
+	case "medium":
+		if time_bound == "yes":
+			print(f"{task} is a medium priority that requires immediate attention today!")
+		else:
+			print(f"{task} is a medium priority do it when free")
+	case "low":
+		if time_bound == "yes":
+			print(f"{task} is a low priority that requires immediate attention today!")
+		else:
+			print(f"{task} is a low priority do it when free")
