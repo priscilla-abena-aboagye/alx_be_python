7521d24a6458903b71222eb9f0dcdb5d6c6d09d3
Abena <priscillaabenaaboagye@gmail.com>
2025-07-26 20:28:58 +0000

    remove parenthesis

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,7 +3,7 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
 	# °C = (°F - 32) x (5/9)
-	celsius = (fahrenheit - 32)  * (FAHRENHEIT_TO_CELSIUS_FACTOR)
+	celsius = (fahrenheit - 32)  * FAHRENHEIT_TO_CELSIUS_FACTOR
 	return celsius
 
 
