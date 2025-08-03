97e898bd59a4011daee5b2334043c5769150ce87
Abena <priscillaabenaaboagye@gmail.com>
2025-08-03 16:58:00 +0000

    fix the spelling

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,7 +10,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(5, -2), 3)
         self.assertEqual(self.calc.add(-4, -6), -10)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 3), 7)
 
     def test_multiply(self): 
